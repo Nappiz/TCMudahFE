b89ade65ec6b55c6269ce720466a97f13703cec2
Badruzzaman Nafiz <badruzzamannafiz@gmail.com>
2025-10-13 00:34:28 +0700

    back

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      { source: "/api/:path*", destination: "https://tcmudahbe.vercel.app/:path*" },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
