9b21da014b89c3972ba585634a218971dc551590
Badruzzaman Nafiz <badruzzamannafiz@gmail.com>
2026-04-25 00:22:28 +0700

    fix proxy

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:8000';
+    const cleanUrl = backendUrl.replace(/\/+$/, '');
 
     return [
       {
         source: '/api/:path*',
-        destination: `${backendUrl}/:path*`,
+        destination: `${cleanUrl}/:path*`,
       },
     ]
   },
